00d378dfda2a3cc6b2daa3219f19ecca7d594494
slitherdmon <105375648+slitherdmon@users.noreply.github.com>
2025-04-08 09:38:34 -0500

    Update uv.config.js

--- apps/deionssearch/active/uv/uv.config.js
+++ apps/deionssearch/active/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/active/uv/uv.handler.js',
-    bundle: '/active/uv/uv.bundle.js',
-    config: '/active/uv/uv.config.js',
-    sw: '/active/uv/uv.sw.js',
+    handler: '/apps/deionssearch/active/uv/uv.handler.js',
+    bundle: '/apps/deionssearch/active/uv/uv.bundle.js',
+    config: '/apps/deionssearch/active/uv/uv.config.js',
+    sw: '/apps/deionssearch/active/uv/uv.sw.js',
 };
