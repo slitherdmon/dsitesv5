036d043ab5e6c1120ed24c77dd2ae5bda1299b32
slitherdmon <105375648+slitherdmon@users.noreply.github.com>
2025-04-09 09:17:32 -0500

    Create uv.config.js

---
+++ apps/tikt0k/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/deionssearch/',
+    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/apps/tikt0k/uv/uv.handler.js',
+    bundle: '/apps/tikt0k/uv/uv.bundle.js',
+    config: '/apps/tikt0k/uv/uv.config.js',
+    sw: '/apps/tikt0k/uv/uv.sw.js',
+};
